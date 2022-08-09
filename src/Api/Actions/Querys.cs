22ec194969d160b3ec9de3498665285a499d9f0a
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-08-09 18:35:00 -0300

    feat: add query class

---
+++ src/Api/Actions/Querys.cs
@@ -0,0 +1,21 @@
+﻿using Api.Models.Request;
+using Api.Models.Response;
+using Application.Query;
+using HotChocolate;
+
+namespace Api.Actions;
+
+public class Querys
+{
+    public IEnumerable<TodoTaskResponse> GetTasks([Service] IGetTodoTasksHandler handler)
+    {
+        var tasks = handler.Execute();
+        return tasks.Select(c => (TodoTaskResponse)c);
+    }
+
+    public TodoTaskResponse GetTask([Service] IGetTodoTaskByIdHandler handler, GetTodoTaskByIdRequest request)
+    {
+        var task = handler.Execute(request.Id);
+        return (TodoTaskResponse)task;
+    }
+}
