1fcab35e3ebf990146c8cbee52fbeeda11e37f1f
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-08-09 18:36:15 -0300

    feat: add handler for insert or update TodoTask

---
+++ src/Application/Command/IInsertOrUpdateTodoTaskHandler.cs
@@ -0,0 +1,8 @@
+﻿using Domain;
+
+namespace Application.Command;
+
+public interface IInsertOrUpdateTodoTaskHandler
+{
+    TodoTask Execute(TodoTask request);
+}
---
+++ src/Application/CommandHandler/InsertOrUpdateTodoTaskHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Command;
+using Domain;
+using Persistence;
+
+namespace Application.CommandHandler;
+
+public class InsertOrUpdateTodoTaskHandler : IInsertOrUpdateTodoTaskHandler
+{
+    private readonly ITodoTaskRepository _repository;
+
+    public InsertOrUpdateTodoTaskHandler(ITodoTaskRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public TodoTask Execute(TodoTask request)
+    {
+        if (request.Id != Guid.Empty)
+        {
+            var databaseItem = _repository.GetById(request.Id);
+            if (databaseItem is null)
+                throw new Exception("Todo task is not found");
+
+            request.FinishedAt = request.IsFinished ? DateTime.UtcNow : null;
+            _repository.Update(request);
+        }
+        else
+        {
+            request.FinishedAt = request.IsFinished ? DateTime.UtcNow : null;
+            request.Id = Guid.NewGuid();
+            request.CreatedAt = DateTime.UtcNow;
+            _repository.Insert(request);
+        }
+        return request;
+    }
+}
