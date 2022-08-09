fc17fc9cfbe828aa6be3a3225d4a940b3bad8967
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-08-09 18:34:49 -0300

    feat: add mutation class

---
+++ src/Api/Actions/Mutations.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models.Request;
+using Api.Models.Response;
+using Application.Command;
+using Domain;
+using HotChocolate;
+
+namespace Api.Actions;
+
+public class Mutations
+{
+    public TodoTaskResponse InsertOrUpdate([Service] IInsertOrUpdateTodoTaskHandler handler, InsertOrUpdateTodoTaskRequest request)
+    {
+        var response = handler.Execute((TodoTask)request);
+
+        if (response is null) return null;
+
+        return (TodoTaskResponse)response;
+    }
+}
